b022145d3b45a25f9ea3c3c61cb51a3fb533bb45
tomas.bozzano <tomycas10@gmail.com>
2025-10-03 16:52:09 -0300

    Add Page notFound

---
+++ src/pages/notFound/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { Template } from "../../components/Template"
+import { NotFound } from "./components/NotFound"
+
+export const NotFoundPage = () => {
+    return (
+        <>
+            <Template>
+                <NotFound />
+            </Template>
+        </>
+    )
+}
---
+++ src/pages/notFound/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export const NotFound = () => {
+  return (
+    <>
+        <h2>404 - Not Found</h2>
+        <p>The page you are looking for does not exist.</p>
+        <Link to="/">Go to Home</Link>
+    </>
+  )
+}
